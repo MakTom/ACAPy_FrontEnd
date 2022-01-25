9f848594d113b7c458be926a53b22b6b132f1931
Mayank <mayanksae@gmail.com>
2022-01-25 15:25:07 -0500

    test

--- src/components/acapy.js
+++ src/components/acapy.js
@@ -9,25 +9,36 @@ const Acapy = () => {
         axios.get(`http://34.201.33.137:3000/topic/connections`)
         .then(res => {
             const conns = res.data.results;
-            console.log("Cons: "+conns);
             setconnections(conns);
-            console.log(connections);
+            setisloading(false);
         })
         .catch(error => {
             console.error(error);
         });   
     }, []);
 
+    acceptrequest(connection_id) {
+        axios.post('http://34.201.33.137:3000/control/acceptrequest', {"connection_id": connection_id})
+          .then(res => {
+            const connections = res.data.results;
+            console.log(connections);
+          })
+          .catch(error => {
+            console.error(error);
+          })
+      }
+
   return ( 
         <div>
-        { isloading && <div>..Loading...</div>}
-    
-        {connections.map(connection => (
-              <li key={connection.connection_id}>{`id: ${connection.connection_id}, rfc23_state: ${connection.rfc23_state}`}</li>
-            ))
-        }
-    
-   
+            { isloading && <div>..Loading...</div>}
+            {connections.map(connection => (
+                <li key={connection.connection_id}>
+                    {`ID: ${connection.connection_id}`}
+                    {`State: ${connection.rfc23_state}`}
+                    connection.rfc23_state==="request-received"? <button onClick={() => this.acceptrequest(connection.connection_id)}>Accept</button>
+                </li>
+                ))
+            }
         </div>
   );
 }
