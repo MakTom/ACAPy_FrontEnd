2acc90fc17dd4e536e4e9705bd396ca68f4502b2
Mayank <mayanksae@gmail.com>
2022-01-25 15:09:19 -0500

    test

--- src/components/acapy.js
+++ src/components/acapy.js
@@ -4,23 +4,23 @@ const Acapy = () => {
 
   const [connections, setconnections] = useState([]);
   useEffect(() => {
-    const fetchconnections = async () => {
       //Update with your ACAPY_Controller IP address
       await axios.get(`http://34.201.33.137:3000/topic/connections`)
         .then(res => {
-          const conns = res.data.results;
-          console.log("Cons: "+conns);
-          setconnections(conns);
-          console.log(connections);
-        }).catch(error => {
-        console.error(error);
-      });
-    }
-    fetchconnections();
-}, []);
+            const conns = res.data.results;
+            console.log("Cons: "+conns);
+            setconnections(conns);
+            console.log(connections);
+        })
+        .catch(error => {
+            console.error(error);
+        });   
+    }, []);
 
   return ( 
-<div></div>
+<div>
+    {connections}
+</div>
   );
 }
 
