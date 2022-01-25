10481045a791d74eee7a7a4e1bf122b293526a7e
Mayank <mayanksae@gmail.com>
2022-01-25 12:33:05 -0500

    test

--- src/components/acapy.js
+++ src/components/acapy.js
@@ -7,7 +7,7 @@ export default class Acapy extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`http://34.201.33.137:3000/connections`)
+    axios.get(`http://34.201.33.137:3000/topic/connections`)
       .then(res => {
         const connections = res.data.results;
         this.setState({ connections });
